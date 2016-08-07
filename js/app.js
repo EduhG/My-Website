64e4226f426c4f9d950ace4eab0c94b9a35b252f
EduhG <edwinnyangena@gmail.com>
2016-08-08 00:11:30 +0300

    Added scroll top button

--- js/app.js
+++ js/app.js
@@ -105,3 +105,27 @@ $("#close").on('click', function (event) {
     event.preventDefault();
     $.Velocity.RunSequence(offCanvasOut);
 });
+
+var $icon = $(".scroll-icn");
+var $down = $("#go-down");
+var $up = $("#go-up");
+
+$icon.velocity({
+    translateY: "10px"
+}, {
+    loop: true
+}).velocity("reverse");
+
+$down.on("click", function () {
+    $up.velocity("scroll", {
+        duration: 2000,
+        easing: "easeInBack"
+    });
+});
+
+$up.on("click", function () {
+    $("body").velocity("scroll", {
+        duration: 2000,
+        easing: "easeInBack"
+    });
+});
